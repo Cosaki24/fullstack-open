e649caf9f3fdb972416db8cea5ba4a9c0589bae9
Collins Kipepe <collysam20@gmail.com>
2025-01-26 13:42:20 +0300

    exercise 1.14 anecdotes step 3

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,5 +1,21 @@
 import {useState} from 'react'
 
+const Anecdote = (props) => {
+  return (
+    <>
+      <h1>{props.title}</h1>
+      <div>{props.anecdote}</div>
+      <div>has {props.votes} votes</div>
+    </>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.action}>{props.text}</button>
+  )
+}
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -15,6 +31,8 @@ const App = () => {
 
   const [selected, setSelected] = useState(0)
   const [voteCount, setVoteCount] = useState(votes)
+  const [mostVoted, setMostVoted] = useState(0)
+  
 
   const chooseRandomAnecdote = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length))
@@ -23,15 +41,22 @@ const App = () => {
   const addVote = () => {
     const copy = [...voteCount]
     copy[selected] += 1
+    setMostVoted(maxIndex(copy))
     setVoteCount(copy)
+  }
+
+  // find the index of a max item in an array
+  const maxIndex = (arr) => {
+    const maxValue = Math.max(...arr)
+    return arr.indexOf(maxValue)
   }
 
   return (
     <>
-      <div>{anecdotes[selected]}</div>
-      <div>has {voteCount[selected]} votes</div>
-      <button onClick={addVote}>vote</button>
-      <button onClick={chooseRandomAnecdote}>next anecdote</button>  
+      <Anecdote title="Anecdote of the day" anecdote={anecdotes[selected]} votes={voteCount[selected]} />
+      <Button action={addVote} text="vote" />
+      <Button action={chooseRandomAnecdote} text="next anecdote" />
+      <Anecdote title="Anecdote with most votes" anecdote={anecdotes[mostVoted]} votes={voteCount[mostVoted]}  />
     </>
   )
     
