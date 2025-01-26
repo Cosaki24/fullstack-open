b4fe22fd3213c5b90a219b0dca3d3b1fbd655ce4
Collins Kipepe <collysam20@gmail.com>
2025-01-26 11:47:02 +0300

    exercise 1.7 unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -12,17 +12,24 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allStats, setAllStats] = useState(0)
+  const [average, setAverage] = useState(0)
 
   const handleGoodClick = () =>{
-    return setGood(good + 1)
+    setAllStats(allStats + 1)
+    setAverage(average + 1)  
+    setGood(good + 1)
   }
 
   const handleNeutralClick = () =>{
-    return setNeutral(neutral + 1)
+    setAllStats(allStats + 1)
+    setNeutral(neutral + 1)
   }
 
-  const handleBadClick = () =>{
-    return setBad(bad + 1)
+  const handleBadClick = () => {
+    setAllStats(allStats + 1)
+    setAverage(average - 1)
+    setBad(bad + 1)
   }
 
   return (
@@ -32,10 +39,13 @@ const App = () => {
       <button onClick={handleNeutralClick}>neutral</button>
       <button onClick={handleBadClick}>bad</button>
       <div>
-        <h2>statistics</h2>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
+        <h1>statistics</h1>
+        <div>good {good}</div>
+        <div>neutral {neutral}</div>
+        <div>bad {bad}</div>
+        <div>all {allStats}</div>
+        <div>average {allStats != 0 ? (average / allStats) : 0}</div>
+        <div>positive {allStats != 0 ? (good / allStats) * 100 : 0} %</div>
       </div>
     </div>
   )
