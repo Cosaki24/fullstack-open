cc08e69efd6df8b01b88cf22f7cc85ed8f219ae5
Collins Kipepe <collysam20@gmail.com>
2025-02-03 18:28:48 +0300

    exercise 2.6 the phonebook step 1

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -16,7 +16,7 @@ const Part = (props) => {
 
 const Content = (props) => {
     const { parts } = props
-    const part = parts.map((part, i) => <Part key={i} name={part.name} exercises={part.exercises} />)
+    const part = parts.map((part) => <Part key={i} name={part.name} exercises={part.exercises} />)
     const total = parts.reduce((acc, curr) => acc += curr.exercises, 0)
 
     return (
@@ -29,7 +29,7 @@ const Content = (props) => {
 
 const Course = (props) => {
     const { courses } = props;
-    const course = courses.map((course, i) => {
+    const course = courses.map((course) => {
         return (
             <div key={i} >
                 <Header title={course.name} />
