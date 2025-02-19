8a93d1e447862e16100a4837d1b0b0b0ac8c35af
Collins Kipepe <collysam20@gmail.com>
2025-02-19 21:12:47 +0300

    exercise 3.7 the phonebook backend step 7

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+var morgan = require('morgan')
 const PORT = 3001
 const app = express()
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let phonebook = [
     {
