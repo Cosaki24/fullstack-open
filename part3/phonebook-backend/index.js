cf01382d9d71c4865e2d1d905a994418f78eff4b
Collins Kipepe <collysam20@gmail.com>
2025-02-19 20:02:01 +0300

    exercise 3.6 the phonebook backend step 6

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -58,6 +58,27 @@ app.post("/api/persons", (request, response)=>{
     const name = request.body.name
     const number = request.body.number
 
+    if(!name || !number){
+        if(!name){
+            return response.status(400).json({
+                error: 'name is missing'
+            })
+        }
+
+        if(!number){
+            return response.status(400).json({
+                error: 'number is missing'
+            })
+        }
+        
+    }
+
+    if(phonebook.some(person => person.name === name)){
+        return response.status(409).json({
+            error: `entry with name '${name}' already exists`
+        })
+    }
+
     const person = {
         id: generateId(),
         name: name,
