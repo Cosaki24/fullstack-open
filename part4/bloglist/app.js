a95464a2099d279ab8604ad4ff343aecffc9b197
Collins Kipepe <collysam20@gmail.com>
2025-03-14 15:19:45 +0300

    exercise 4.2 blog list step 2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogRouter = require('./controllers/blog')
+
+mongoose.set('strictQuery', false)
+
+logger.info('Connecting to MongoDB...')
+
+mongoose
+	.connect(config.MONGODB_URL)
+	.then(() => {
+		logger.info('Connected to MongoDB')
+	})
+	.catch((error) => {
+		logger.error('Error connection to mongoDB: ', error.message)
+	})
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,31 +1,7 @@
-const express = require('express')
-const middleware = require('./utils/middleware')
-const logger = require('./utils/logger')
+const app = require('./app')
 const config = require('./utils/config')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const blogRouter = require('./controllers/blog')
-
-mongoose.set('strictQuery', false)
-
-logger.info('Connecting to MongoDB...')
-
-mongoose
-	.connect(config.MONGODB_URL)
-	.then(() => {
-		logger.info('Connected to MongoDB')
-	})
-	.catch((error) => {
-		logger.error('Error connection to mongoDB: ', error.message)
-	})
-
-app.use(cors())
-app.use(express.json())
-app.use(middleware.requestLogger)
-app.use('/api/blogs', blogRouter)
-
-
-app.listen(PORT, () => {
+const logger = require('./utils/logger')
+ 
+app.listen(config.PORT, () => {
 	logger.info(`Server running on port ${config.PORT}`)
 })
