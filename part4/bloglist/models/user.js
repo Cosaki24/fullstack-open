b551e3e8dcdd5174a00dcaf37684d6b2e6b14bcd
Collins Kipepe <collysam20@gmail.com>
2025-03-24 21:22:38 +0300

    defining userSchema

---
+++ part4/bloglist/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+	name: { type: String, required: [true, 'name is required'] },
+	username: {
+		type: String,
+		required: [true, 'username is required'],
+		unique: true,
+	},
+	passwordHash: String,
+	blogs: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Blog',
+		},
+	],
+})
+
+userSchema.set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+		delete returnedObject.passwordHash
+	},
+})
+
+module.exports = mongoose.Model('User', userSchema)
